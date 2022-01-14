17754a463769a653adec8783d81584635ad4b2b2
supertgo <thiagoroberto2003@gmail.com>
2022-01-14 05:57:14 -0300

    chore(jest): add svg loader to jest config js
    add svg loader to jest config js to avoid import svg errors

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,8 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    '.+\\.(svg|css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub'
   }
-}
+};
