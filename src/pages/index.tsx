832f188630e614648bed7e8434025a4f8caeda23
supertgo <thiagoroberto2003@gmail.com>
2022-01-15 11:15:29 -0300

    feat(api): :sparkles: add axios and mirage js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
 import Dashboard from 'components/Dashboard';
 import Header from 'components/Header';
+import TransactionsTable from 'components/TransactionsTable';
+
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.get('/api/transactions', () => {
+      return [
+        { id: 1, name: 'Inception', year: 2010 },
+        { id: 2, name: 'Interstellar', year: 2014 },
+        { id: 3, name: 'Dunkirk', year: 2017 }
+      ];
+    });
+  }
+});
 
 export default function Home() {
   return (
     <>
       <Header />
       <Dashboard />
+      <TransactionsTable />
     </>
   );
 }
