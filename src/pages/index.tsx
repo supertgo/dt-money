d57ad68dafc68cea2badbabf8ce287c91957aad1
supertgo <thiagoroberto2003@gmail.com>
2022-01-16 10:47:08 -0300

    feat(api): :sparkles: add miragejs seed to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,46 @@
 import Dashboard from 'components/Dashboard';
 import Header from 'components/Header';
 import NewTransactionModal from 'components/NewTransactionModal';
-import TransactionsTable from 'components/TransactionsTable';
 
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { useState } from 'react';
 
 createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: '1',
+          title: 'Freelance de Webisite',
+          type: 'deposit',
+          category: 'dev',
+          amount: 6000,
+          createdAt: new Date('2022-02-12 09:00:00')
+        },
+        {
+          id: '2',
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: -1100,
+          createdAt: new Date('2022-02-14 11:00:00')
+        }
+      ]
+    });
+  },
   routes() {
     this.get('/api/transactions', () => {
-      return [
-        { id: 1, name: 'Inception', year: 2010 },
-        { id: 2, name: 'Interstellar', year: 2014 },
-        { id: 3, name: 'Dunkirk', year: 2017 }
-      ];
+      return this.schema.all('transaction');
+    });
+
+    this.post('/api/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     });
   }
 });
@@ -37,7 +64,6 @@ export default function Home() {
         onRequestClose={closeOpenNewTransactionModal}
       />
       <Dashboard />
-      <TransactionsTable />
     </>
   );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -43,6 +43,27 @@ const GlobalStyles = createGlobalStyle`
       opacity: 0.6;
       cursor: not-allowed;
     }
+
+    .react-modal-overlay {
+      background: rgba(0, 0, 0, 0.5);
+      position: fixed;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .react-modal-content {
+      width: 100%;
+      max-width: 57.6rem;
+      background: ${theme.colors.background};
+      padding: ${theme.spacings.xlarge};
+      position: relative;
+      border-radius: ${theme.border.radius};
+    }
   `}  
 `;
 
