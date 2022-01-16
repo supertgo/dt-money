e5a19ec82f0c585523ca5371e988aa2233655104
supertgo <thiagoroberto2003@gmail.com>
2022-01-16 10:23:16 -0300

    refactor(components): :recycle: refact Dashboard component

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,9 +1,11 @@
 import Summary from 'components/Summary';
+import TransactionsTable from 'components/TransactionsTable';
 import * as S from './styles';
 
 const Dashboard = () => (
   <S.Wrapper>
     <Summary />
+    <TransactionsTable />
   </S.Wrapper>
 );
 
--- src/components/Dashboard/test.tsx
+++ src/components/Dashboard/test.tsx
@@ -16,6 +16,22 @@ describe('<Dashboard />', () => {
 
     expect(screen.getByTestId('Mock Summary')).toBeInTheDocument();
     expect(container.firstChild).toMatchInlineSnapshot(`
+      .c1 {
+        margin-top: 6.4rem;
+      }
+
+      .c2 {
+        width: 100%;
+        border-spacing: 0 0.8rem;
+      }
+
+      .c3 {
+        color: #969CB3;
+        padding: 1.6rem 3.2rem;
+        text-align: left;
+        line-height: 2.4rem;
+      }
+
       .c0 {
         max-width: 112rem;
         margin: 0 auto;
@@ -28,6 +44,45 @@ describe('<Dashboard />', () => {
         <div
           data-testid="Mock Summary"
         />
+        <main
+          class="c1"
+        >
+          <table
+            class="c2"
+          >
+            <thead
+              class=""
+            >
+              <tr
+                class=""
+              >
+                <th
+                  class="c3"
+                >
+                  Título
+                </th>
+                <th
+                  class="c3"
+                >
+                  Valor
+                </th>
+                <th
+                  class="c3"
+                >
+                  Categoria
+                </th>
+                <th
+                  class="c3"
+                >
+                  Data
+                </th>
+              </tr>
+            </thead>
+            <tbody
+              class=""
+            />
+          </table>
+        </main>
       </main>
     `);
   });
