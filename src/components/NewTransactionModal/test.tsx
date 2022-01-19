4a04aeb541cf21e7d00f5303aeeed34fb111f6c1
supertgo <thiagoroberto2003@gmail.com>
2022-01-19 16:21:56 -0300

    :white_check_mark: add another test to NewTransactionModal

--- src/components/NewTransactionModal/test.tsx
+++ src/components/NewTransactionModal/test.tsx
@@ -1,4 +1,6 @@
-import { screen, waitFor } from '@testing-library/react';
+import { screen, waitFor, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { TransactionsProvider } from 'hooks/TransactionContext';
 import ReactModal from 'react-modal';
 import { renderWithTheme } from 'utils/tests/helpers';
 
@@ -9,13 +11,41 @@ ReactModal.setAppElement(document.createElement('div'));
 describe('<NewTransactionModal />', () => {
   it('should render the modal info when it is closed', () => {
     const mockOnRequestClose = jest.fn();
-    const { container } = renderWithTheme(
+    renderWithTheme(
       <NewTransactionModal isOpen={false} onRequestClose={mockOnRequestClose} />
     );
 
     expect(mockOnRequestClose).not.toBeCalled();
     expect(screen.queryByText('Cadastrar transação')).not.toBeInTheDocument();
-    expect(container).toMatchInlineSnapshot(`<div />`);
+  });
+
+  it('should render the modal info and close with real data ', () => {
+    const mockOnRequestClose = jest.fn();
+    renderWithTheme(
+      <TransactionsProvider>
+        <NewTransactionModal
+          isOpen={true}
+          onRequestClose={mockOnRequestClose}
+        />
+      </TransactionsProvider>
+    );
+
+    expect(mockOnRequestClose).not.toBeCalled();
+    expect(screen.queryByText('Cadastrar transação')).toBeInTheDocument();
+
+    const submitButton = screen.getByRole('button', { name: /Cadastrar/i });
+
+    userEvent.click(submitButton);
+
+    const inputTitulo = screen.getByPlaceholderText('Título');
+    const inputCategory = screen.getByPlaceholderText('Categoria');
+
+    fireEvent.change(inputTitulo, { target: 'Transação' });
+    fireEvent.change(inputCategory, { target: 'Categoria' });
+
+    waitFor(() => {
+      expect(mockOnRequestClose).toBeCalled();
+    });
   });
 
   it('should render the modal info when it is open', () => {
